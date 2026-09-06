40dcf706d70edbbd009612d4e3bae5929a222902
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-09-06 15:22:05 +0900

    Program.cs：DI設定追加・実行クラスインスタンス生成と実行 #3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace ConsoleApp;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Logic.Repositories;
+using Logic.Interfaces.Repositories;
+using Logic.Applictions;
+using Logic.Interfaces.Applictions;
+
+namespace ConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((Context, sevice) =>
+            {
+                // DI設定
+                sevice.AddTransient<IFileRepository, FileRepository>();
+                sevice.AddTransient<ISampleApplication, SampleApplication>();
+
+                // 実行クラス
+                sevice.AddSingleton<App>();
+            })
+            .Build();
+
+        var app = host.Services.GetRequiredService<App>();
+        app.Run();
     }
 }
