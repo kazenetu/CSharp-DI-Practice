32328d25158d404106a7c0ffad8d030b8ca122aa
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-09-12 15:29:35 +0900

    ServiceCollectionExtension.cs：LogicクラスDI設定用「IServiceCollection拡張クラス」追加 #7

---
+++ Logic/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Logic.Extensions;
+
+/// <summary>
+/// IServiceCollection拡張クラス
+/// </summary>
+public static class ServiceCollectionExtension
+{
+   /// <summary>
+   /// DIコンテナ設定：ロジック部の登録
+   /// </summary>
+   /// <param name="service">IServiceCollectionインスタンス</param>
+   /// <returns>IServiceCollectionインスタンsy</returns>
+   public static IServiceCollection AddLogicDI(this IServiceCollection service)
+   {
+      return service;
+   }
+}
